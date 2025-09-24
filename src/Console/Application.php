14f8e600265f05f1ad0e557455b2c9494461b04a
Helmi <helmi.prasetyo12@gmail.com>
2025-09-24 21:58:07 +0700

    Add version command and update application version
    Introduces a new VersionCommand to display version information for Yii2 Fixer, including PHP and PHP-CS-Fixer versions. Updates the application version to 1.0.1 and improves autoloading in the entry script. Also restricts PHP requirement in composer.json to ^7.4.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -4,13 +4,15 @@ namespace zhikariz\yii2\fixer\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
 use zhikariz\yii2\fixer\Console\Commands\FixCommand;
+use zhikariz\yii2\fixer\Console\Commands\VersionCommand;
 
 class Application extends BaseApplication
 {
     public function __construct()
     {
-        parent::__construct('Yii2 Fixer By zhikariz', '1.0.0');
+        parent::__construct('Yii2 Fixer By zhikariz', '1.0.1');
 
         $this->add(new FixCommand());
+        $this->add(new VersionCommand());
     }
 }
---
+++ src/Console/Commands/VersionCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace zhikariz\yii2\fixer\Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class VersionCommand extends Command
+{
+    protected static $defaultName = 'version';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Show version information');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Yii2 Fixer By zhikariz 1.0.1');
+        $output->writeln('PHP version: ' . PHP_VERSION);
+        $output->writeln('PHP-CS-Fixer version: ' . \PhpCsFixer\Console\Application::VERSION);
+
+        return Command::SUCCESS;
+    }
+}
